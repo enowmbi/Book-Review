1fb119b2103fbb674f394fe3bdf496ad34ba25ba
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 17:29:58 +0100

    Modify books request specs --added patch section

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -38,21 +38,35 @@ RSpec.describe "Books", type: :request do
     end
   end
 
-  describe "POST/create" do 
+  describe "POST /books" do 
     let(:valid_params) {{ book: {title: "Delivered? An Exegetical Investigation", description: "What is deliverance? what does it mean to be delivered? How are the words delivered and deliverance defined and used throughout the New Testament? Can a believer be indwelt with demons?",author: "Dr. Shawn Smith"} } }   
     before(:each) { post books_url,params: valid_params}
 
-    it "returns a response with http status created" do 
-       expect(response).to have_http_status(302)
+    it "returns a response with http status redirected" do 
+      expect(response).to have_http_status(302)
     end
 
     it "returns a response with HTML content type" do 
-        expect(response.content_type).to eq('text/html')
+      expect(response.content_type).to eq('text/html')
     end
 
     it "increments book collections by 1" do 
-       expect{post books_url,params: valid_params}.to change(Book,:count).by(1)
+      expect{post books_url,params: valid_params}.to change(Book,:count).by(1)
     end
+  end
+
+  describe "PATCH/PUT /books/1" do 
+    let(:valid_params) {{ book: {author: "Shawn Smith DD,ThD"} } }   
+    before(:each) { patch book_url(Book.last),params: valid_params}
+
+    it "returns a response with http status redirected" do 
+      expect(response).to have_http_status(302)
+    end
+
+    it "returns a response with HTML content type" do 
+      expect(response.content_type).to eq('text/html')
+    end
+
   end
 
 
