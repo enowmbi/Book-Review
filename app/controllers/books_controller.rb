fb2dae06db12efe99bc4fd4abc56dc5434533a6b
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 09:41:08 +0100

    Modify books controller --added show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,14 @@ class BooksController < ApplicationController
    @books = Book.all
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
 
 
+
+  private 
+  def book_params
+    params.require(:book).permit(:title,:description,:author)
+  end
 end
