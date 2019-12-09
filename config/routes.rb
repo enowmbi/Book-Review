5bdec409e44bd628c5603edc19773f2639a7fd8c
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 18:27:07 +0100

    Modify routes.rb --added entry for destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
    root "books#index"
-   resources :books, only: [:index,:show,:new,:create,:edit,:update]
+   resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
 
 end
