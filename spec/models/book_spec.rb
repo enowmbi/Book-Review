cfdb6cae25d3ec7314a85ac78d90fc2b9d2dd32d
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 05:11:43 +0100

    Modify book model spec -- added examples for validation

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+  before(:all){ @book = FactoryBot.build(:book) }
+
+  describe "validation" do 
+    it "is invalid without a title" do
+      @book.title = ""
+      expect(@book).not_to be_valid
+    end
+
+    it "is invalid without a description" do 
+      @book.description =""
+      expect(@book).not_to be_valid
+    end
+
+    it "is invalid without an author" do 
+      @book.author = ""
+      expect(@book).not_to be_valid
+    end
+
+  end
+  describe "associations" do 
+
+  end
+
+
+
 end
