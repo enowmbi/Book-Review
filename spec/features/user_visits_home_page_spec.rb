f9217c7e7d8afaa58a840c38fe909b8f7139b9fa
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 06:25:29 +0100

    Modify visit_home_page to user_visits_home_page

--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Book", type: :feature do 
 
-  scenario "visit home page" do 
+  scenario "user visits home page" do 
     visit root_path
+    expect(page).to have_current_path(root_path)
     expect(page).to have_content("Book Review") 
   end
 
