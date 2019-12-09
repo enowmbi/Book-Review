9304400dc327f3e79ed4d89c44c667d48b4f4503
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 09:45:30 +0100

    Modify user visits home page feature spec -- added click link

--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Book", type: :feature do 
+RSpec.describe "user visits home page", type: :feature do 
 
-  scenario "user visits home page" do 
+  scenario "successfully" do 
     visit root_path
     expect(page).to have_current_path(root_path)
     expect(page).to have_content("Book Review") 
+    expect(page).to have_link('Making Marriage Work')
   end
 
 end
