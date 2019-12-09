f7234b2235c04784f641d1264ca5d2521489d3a6
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 00:57:46 +0100

    Modify routes.rb --whitelisted destroy action in categories resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
    root "books#index"
    resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
-   resources :categories, only: [:index,:show,:new,:create,:edit,:update]
+   resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy]
 
 end
