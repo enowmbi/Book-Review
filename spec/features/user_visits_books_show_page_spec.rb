af94402f09a388045be2036cd01b9d9f48bacf86
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 16:53:08 +0100

    Modify user visits book show page features

--- spec/features/user_visits_books_show_page_spec.rb
+++ spec/features/user_visits_books_show_page_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "user visits book show page" , type: :feature do
 
-  before(:all) { @book = FactoryBot.create(:book) }
-
+  before(:all){@book= FactoryBot.create(:book) } 
   scenario "successfully" do 
     visit root_path
-    # visit book_path(@book)
+    expect(page).to have_link("#{@book.title}")
+    puts "title is : #{@book.id} - #{@book.title}"
     click_link @book.title
-    # expect(page).to redirect_to(book_path(@book))
     expect(page).to have_current_path(book_path(@book))
     expect(page).to have_content(@book.title)
   end
