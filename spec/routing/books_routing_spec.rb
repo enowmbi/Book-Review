485ba2b425a072fe1d603fcbc3096f766cf5bd46
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 09:43:31 +0100

    Modify books routing specs --added routing to #show

--- spec/routing/books_routing_spec.rb
+++ spec/routing/books_routing_spec.rb
@@ -5,13 +5,16 @@ RSpec.describe BooksController,type: :routing do
   describe "routing" do 
 
     it "routes to root" do 
-     expect(:get => "/").to route_to("books#index")
+      expect(:get => "/").to route_to("books#index")
     end
 
-    it "routes to index" do 
+    it "routes to #index" do 
       expect(:get => "/books").to route_to("books#index")
     end
 
+    it "routes to #show" do 
+      expect(:get => "/books/1").to route_to("books#show", :id => "1")
+    end
 
   end
 
