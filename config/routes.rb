491ce195c6324b3edfb5874c26de7e8143aea960
Enow Mbi <benowmbi@yahoo.com>
2019-12-09 17:29:09 +0100

    Modify routes.rb --added entry for update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
    root "books#index"
-   resources :books, only: [:index,:show,:new,:create,:edit]
+   resources :books, only: [:index,:show,:new,:create,:edit,:update]
 
 end
