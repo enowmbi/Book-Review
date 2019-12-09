db99e47333133120147e9d7f481537f227c70af6
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 00:31:35 +0100

    Modify categories controller --added update action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,15 +28,28 @@ class CategoriesController < ApplicationController
 
   end
 
-  private
-  def set_category
-    @category = Category.find(params[:id])
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html{redirect_to @category, notice: "Category successfully updated"}
+        format.json{render :show,status: :ok,location: @category}
+      else
+        format.html{render :edit}
+        format.json{render json: @category.errors,status: :unprocessable_entity}
+      end
+
+    end
   end
 
-  def category_params
-    params.require(:category).permit(:name,:description)
+    private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name,:description)
+    end
+
+
+
   end
-
-
-
-end
