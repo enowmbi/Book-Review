223e3abbfbc3b27879f56d9857219162f87d8a74
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 19:21:10 +0100

    Modify categories routing spec --added show action routing

--- spec/routing/categories_routing_spec.rb
+++ spec/routing/categories_routing_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :routing do 
 
   describe "routing" do 
-  it "routes to #index" do 
+    it "routes to #index" do 
       expect(:get => '/categories').to route_to("categories#index")
-  end
+    end
+
+    it "routes to #show" do 
+      expect(:get => '/categories/1').to route_to("categories#show", :id=> "1")
+    end
+
   end
 end
