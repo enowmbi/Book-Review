e0e9453895f64cd668e177df4ba61aece5106771
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 08:58:07 +0100

    Modify categories request specs --added examples in the get/index category

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -3,17 +3,22 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :request do 
 
 describe "GET/index" do 
-  
+   before(:all) do 
+      @@category = FactoryBot.create(:category)
+      get "/categories"
+   end
+
   it "returns a response with http status of success" do 
-      
+    expect(response).to have_http_status(:success)      
   end
 
   it "returns a response with HTML content type " do 
-
-
+     expect(response.content_type).to eq('text/html')
   end
 
 
 end
+
+after(:all){ @@category.destroy }
 
 end
