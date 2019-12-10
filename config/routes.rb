f6a5c79cb5adf9723b9425a78d45e4e2db269627
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 21:36:52 +0100

    Modify routes.rb --added entry for new action for categories resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
    root "books#index"
    resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
-   resources :categories, only: [:index,:show,:edit]
+   resources :categories, only: [:index,:show,:new,:edit]
 
 end
