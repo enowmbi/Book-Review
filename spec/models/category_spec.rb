795f131ee067328d31fb528e933d2c1cbaabdee4
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 10:19:22 +0100

    Modify category model specs --added validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) { @category ||= FactoryBot.build(:category)}
+  describe "validations" do 
+      
+    it "fails without a category name" do
+       @category.name =""
+       expect(@category).not_to be_valid
+    end
+
+    it "fails without a category description" do 
+     @category.description =""
+     expect(@category).not_to be_valid
+  end
+
+    xit "fails when category name already exists" do
+        if Category.find_by(name: "#{@category.name}")
+           expect(@category).not_to be_valid
+        else
+            expect(@category).to be_valid
+        end
+    end
+
+
+
+  
+  end
+
+  describe "associations" do 
+
+  end
+
+  describe "graceful deletion" do 
+
+  end
+
 end
