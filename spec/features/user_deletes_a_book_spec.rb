3e524e0fae70154c04478a6215595066f5ebbbfc
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 07:43:44 +0100

    Modify user deletes a book features spec -- enforce transactional examples and contexts

--- spec/features/user_deletes_a_book_spec.rb
+++ spec/features/user_deletes_a_book_spec.rb
@@ -2,17 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "user deletes a book", type: :feature do
 
-  before(:all){@book = FactoryBot.create(:book)}
 
   scenario "successfully" do 
+    @book = FactoryBot.create(:book)
     visit book_path @book 
     expect(current_path).to eq(book_path(@book))
     click_on "Destroy"
     expect(current_path).to eq(books_path)
 
   end
-
-
-
 
 end
