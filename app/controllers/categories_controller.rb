555b7d9d40442f29b2cc3260d53628f1fab2d17f
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 22:19:43 +0100

    Modify categories controller -- implemented create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,36 @@
 class CategoriesController < ApplicationController
- before_action :set_category, only: [:show,:edit,:update,:destroy]
+  before_action :set_category, only: [:show,:edit,:update,:destroy]
   def index
-     @categories = Category.all
+    @categories = Category.all
   end
 
   def show
-
   end
 
   def new
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    respond_to do |format|
+      if @category.save
+        format.html {redirect_to @category ,notice: "Category was successfully created"}
+        format.json {render show,location: @category,status: :created}
+      else
+        format.html {render :new}
+        format.json {render json: @category.errors,status: :unprocessable_entity}
+      end
+    end
+  end
+
   private
   def set_category
-     @category = Category.find(params[:id])
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name,:description)
   end
 
 
