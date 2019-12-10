428f25ecf12ef662313992e14985bb01c8d005ca
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 08:40:47 +0100

    Add categories request specs

---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :request do 
+
+describe "GET/index" do 
+  
+  it "returns a response with http status of success" do 
+      
+  end
+
+  it "returns a response with HTML content type " do 
+
+
+  end
+
+
+end
+
+end
