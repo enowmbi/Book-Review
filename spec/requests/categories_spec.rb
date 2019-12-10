925968a9d8385f383921abc1cc1d537680ae9ffc
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 22:20:43 +0100

    Modify categories request specs -- added spec for create action

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -43,6 +43,21 @@ RSpec.describe CategoriesController, type: :request do
 
   end
 
+  describe "POST/create" do 
+    let(:valid_params){{category:{name: "Time Travel",description: "All time travel issues"} }}
+    before(:each){post categories_path,params: valid_params}
+
+    it "returns a response with http status of redirected(302)" do 
+      expect(response).to have_http_status(302)
+    end
+
+    it "returns a response with HTML content type" do 
+      expect(response.content_type).to eq("text/html")
+    end
+
+
+  end
+
   after(:all){ @@category.destroy }
 
 end
