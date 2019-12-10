eebec208c89597c9ba50b171c958e0f2885003d7
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 19:20:47 +0100

    Modify categories requests specs -- added get/show/1

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,23 +2,35 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :request do 
 
-describe "GET/index" do 
-   before(:all) do 
+  describe "GET/index" do 
+    before(:all) do 
       @@category = FactoryBot.create(:category)
       get "/categories"
-   end
+    end
 
-  it "returns a response with http status of success" do 
-    expect(response).to have_http_status(:success)      
+    it "returns a response with http status of success" do 
+      expect(response).to have_http_status(:success)      
+    end
+
+    it "returns a response with HTML content type " do 
+      expect(response.content_type).to eq('text/html')
+    end
+
   end
 
-  it "returns a response with HTML content type " do 
-     expect(response.content_type).to eq('text/html')
+  describe "GET/show/1" do 
+    before(:each) {get "/categories/#{Category.first.id}"}
+
+    it "returns a response with http status of success" do 
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns an html content type" do 
+      expect(response.content_type).to eq('text/html')
+    end
   end
 
 
-end
-
-after(:all){ @@category.destroy }
+  after(:all){ @@category.destroy }
 
 end
