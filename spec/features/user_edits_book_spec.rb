0a9d791296cecbd8b5ea4ccb4cea081480a6d87b
Enow Mbi <benowmbi@yahoo.com>
2019-12-10 07:45:30 +0100

    Modify user edits book features specs -- enforced transactional examples and contexts

--- spec/features/user_edits_book_spec.rb
+++ spec/features/user_edits_book_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "user edits a book", type: :feature do 
-  before(:all){ @book = FactoryBot.create(:book) }
 
-   scenario "successfully" do 
+  scenario "successfully" do 
+    @book = FactoryBot.create(:book)
     visit book_path @book
     expect(current_path).to eq(book_path(@book))
     click_on "Edit"
     expect(current_path).to eq(edit_book_path(@book))
-   end
+  end
 
 end
