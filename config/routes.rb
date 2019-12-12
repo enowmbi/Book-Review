47ee944247994a346c65a573eb2de0a986ca1b3b
Enow Mbi <benowmbi@yahoo.com>
2019-12-12 11:33:46 +0100

    Modify routes.rb --added devise_for: users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
    root "books#index"
    resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
    resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy]
