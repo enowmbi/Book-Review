ca0b668a1c7fbff12aef8b504963e4d840b8328b
Enow Mbi <benowmbi@yahoo.com>
2019-12-12 13:45:20 +0100

    Modify routes.rb --formatted lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-   root "books#index"
-   resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
-   resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy]
+  devise_for :user
+  root "books#index"
+  resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
+  resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy]
 
 end
