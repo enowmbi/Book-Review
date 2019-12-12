7b8d0afce6a5f6e4a5c54ce81a9df630c1bf7a28
Enow Mbi <benowmbi@yahoo.com>
2019-12-12 15:06:05 +0100

    Modify user model spec -- validation of admin?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-
-end
+  describe "validation" do 
+    it "fails without admin? column" do 
+      user = FactoryBot.build(:user,admin?: nil)
+      expect(user).not_to be_valid
+    end
+    end
+  end
