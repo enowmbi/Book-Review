d6be18294972c91b6ed3e37d1991f875787dd62b
Enow Mbi <benowmbi@yahoo.com>
2019-12-12 16:55:48 +0100

    Modify books controller -- whitelist image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,6 +57,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title,:description,:author)
+    params.require(:book).permit(:title,:description,:author,:image)
   end
 end
