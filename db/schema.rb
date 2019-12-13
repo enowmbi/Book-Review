23ffa9cc65a6be1fcbef6ebcb6a7e32639a93eb6
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 21:02:01 +0100

    Modify schema by adding user_id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_002227) do
+ActiveRecord::Schema.define(version: 2019_12_13_195700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_002227) do
     t.bigint "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
