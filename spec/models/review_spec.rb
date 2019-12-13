668f583944e4c3cb18d1f01ed4ce2513d5878c38
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 10:43:20 +0100

    Modify review model spec -- specs for validation of rating and comments

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation" do 
+    review = FactoryBot.build(:review)
+    it "fails without rating" do 
+      review.rating =''
+      expect(review).not_to be_valid
+    end
+
+    it "fails without numeric rating " do 
+      review.rating ='abcd'
+      expect(review).not_to be_valid
+    end
+
+    it "fails with a rating of out of range(1..5)" do
+      review.rating = 15
+      expect(review).not_to be_valid
+    end
+
+    it "fails without comment" do 
+      review.comment = ''
+      expect(review).not_to be_valid
+    end
+
+    it "fails without a user id" do 
+      review.user_id = nil
+      expect(review).not_to be_valid
+    end
+
+    it "fails without a book id" do 
+      review.book_id = nil
+      expect(review).not_to be_valid
+    end
+  end
+
 end
