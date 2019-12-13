21c13af3220385d4dc514ac42107836505ec51c5
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 10:44:57 +0100

    Add reviews routing specs

---
+++ spec/routing/reviews_routing_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ReviewsController, type: :routing do
+
+  describe "routing" do 
+    it "doesn't route to reviews/index"
+
+    it "routes to #book/id/reviews"  
+
+
+    it "doesn't route to #new"
+    it "routes to book/id/reviews/new"
+
+
+  end
+
+
+end
