edfdb2a8d767a22b7b2a81db88bec3703d2c1a5e
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 23:03:17 +0100

    Modify routes --added nested review routes to books

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/index'
   devise_for :user
   root "books#index"
-  resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy]
+  resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy] do 
+     resources :reviews
+  end
+
   resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy]
 
 end
