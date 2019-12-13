e1e0aeb63fd71de567f9cc13709a985596ab5c95
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 23:09:29 +0100

    Modify routes.rb -- with respect to nested reviews resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/edit'
-  get 'reviews/update'
-  get 'reviews/index'
   devise_for :user
   root "books#index"
   resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy] do 
