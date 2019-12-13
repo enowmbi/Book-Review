0f8266355151bdc8df75104dc282ab0cade2365d
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 10:57:43 +0100

    Modify reviews routing specs --added more exampesl

--- spec/routing/reviews_routing_spec.rb
+++ spec/routing/reviews_routing_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe ReviewsController, type: :routing do
 
   describe "routing" do 
-    it "doesn't route to reviews/index"
+    it "doesn't route to reviews/index" do
+       expect(:get => 'reviews').not_to route_to('reviews#index')
+    end
 
-    it "routes to #book/id/reviews"  
 
-
-    it "doesn't route to #new"
-    it "routes to book/id/reviews/new"
-
+    it "doesn't route to #new" do 
+      expect(:get => 'reviews/new').not_to route_to('reviews#new')
+    end
 
   end
 
