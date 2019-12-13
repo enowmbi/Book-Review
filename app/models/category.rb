7d53f27d73434b78862d4dbbaf00becdb4a49e99
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 21:24:27 +0100

    Modify category model --updated book model for dependent destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 
-  has_many :books
+  has_many :books, dependent: :destroy
 
   validates :name, presence: true #,uniqueness: {scope: :name}
   validates :description, presence: true
