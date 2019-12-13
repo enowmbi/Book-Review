e0a9a0a751bd3562fe1cb48c1b6a109c95760071
Enow Mbi <benowmbi@yahoo.com>
2019-12-13 21:09:08 +0100

    Modify schema.rb -- changed review rating from integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_195700) do
+ActiveRecord::Schema.define(version: 2019_12_13_200330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_195700) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
+    t.float "rating"
     t.text "comment"
     t.bigint "book_id"
     t.datetime "created_at", null: false
