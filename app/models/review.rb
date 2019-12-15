82fa1429019d0f9cbf079d1a70359a97a6e866b0
Enow Mbi <benowmbi@yahoo.com>
2019-12-16 00:47:58 +0100

    Modify review model -- added default scope

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,6 @@ class Review < ApplicationRecord
   validates :rating, presence: true, numericality: {only_integers: true,greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :comment, presence: true
 
-  default_scope {order(updated_at: :desc)}
+  default_scope ->{order(updated_at: :desc)}
   
 end
