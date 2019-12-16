9c3cb2eaa21777f90360aaeb53dd4d01b7c6d28b
Enow Mbi <benowmbi@yahoo.com>
2019-12-16 02:39:58 +0100

    Modify routes.rb -- allow each member category to have books

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   devise_for :user
   root "books#index"
   resources :books, only: [:index,:show,:new,:create,:edit,:update,:destroy] do 
-     resources :reviews
+    resources :reviews
   end
 
-  resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy]
+  resources :categories, only: [:index,:show,:new,:create,:edit,:update,:destroy] do 
+     member do 
+         get :books
+     end
+  end
 
 end
