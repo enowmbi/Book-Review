6dd441db1cf9ef3b4a3e355a9967e08059c083f0
Enow Mbi <benowmbi@yahoo.com>
2019-12-17 01:07:21 +0100

    Modify review model --added counter_cache for user and book

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :book
-  belongs_to :user
+  belongs_to :book,counter_cache: true
+  belongs_to :user,counter_cache: true
 
   validates :rating, presence: true, numericality: {only_integers: true,greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :comment, presence: true
