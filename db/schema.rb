132769f424e06176e6599e96b47bd7f1a67dd66b
Enow Mbi <benowmbi@yahoo.com>
2019-12-17 01:08:33 +0100

    Modify schema.rb --added books_count,reviews_count in category,user and books tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_023740) do
+ActiveRecord::Schema.define(version: 2019_12_16_232125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_023740) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.integer "user_id"
+    t.integer "reviews_count", default: 0, null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -51,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_023740) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "books_count", default: 0, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -75,6 +77,8 @@ ActiveRecord::Schema.define(version: 2019_12_15_023740) do
     t.string "username"
     t.string "first_name"
     t.string "last_name"
+    t.integer "books_count", default: 0, null: false
+    t.integer "reviews_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
