82fe6c8edbe686de03b4f2d4228959eb5d13b62b
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 10:41:38 +0100

    Modify review.rb --add validation for uniqueness of user_id and book_id in review model --preventing users from reviewing a book more than once

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
   validates :rating, presence: true, numericality: {only_integers: true,greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :comment, presence: true
 
+  validates :user_id, uniqueness: {scope: :book_id, message: "You are entitled to only one review for each book"}
+
   default_scope ->{order(updated_at: :desc)}
   
 end
