a2786792867cbc2e61c0554cbb23b623597d46f8
Enow Mbi <benowmbi@yahoo.com>
2020-04-07 20:37:54 +0100

    Modify javascript/packs/application.js --remove font-awesome entry

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,10 +14,8 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-
-// require("trix")
-// require("@rails/actiontext")onsole.log('Hello World from Webpacker')
+require("trix");
+require("@rails/actiontext");
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
@@ -26,5 +24,5 @@ require("channels");
 import "bootstrap";
 import "jquery";
 import "popper.js";
-import "font-awesome";
 import "raty-js";
+
