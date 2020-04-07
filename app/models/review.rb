1b1c7cdccb6cd686757cefa54931a4d843384a92
Enow Mbi <benowmbi@yahoo.com>
2020-04-07 23:36:33 +0100

    modify review.rb --remove validation of number_field

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,6 @@ class Review < ApplicationRecord
   belongs_to :book,counter_cache: true
   belongs_to :user,counter_cache: true
 
-  validates :rating, presence: true, numericality: {only_integers: true,greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :comment, presence: true
 
   validates :user_id, uniqueness: {scope: :book_id, message: "You are entitled to only one review for each book"}
